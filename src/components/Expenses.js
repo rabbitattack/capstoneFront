915b27b1bf98d60924082aca0f36026355232482
Jedi77 <jprentup@hotmail.com>
2018-11-05 10:08:26 -0700

    front end first

---
+++ src/components/Expenses.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Expenses extends React.Component {
+    render() {
+        return (
+            <div className="sixpense">
+                <header className="App-header">
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+            </p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Expenses
